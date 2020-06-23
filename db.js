f9ead0ddd92f4c5555102efc115ddac44477805a
Jon Hand <T9JRH@int.mcmaster.com>
2020-06-23 15:35:44 -0700

    working init put and get by index

--- db.js
+++ db.js
@@ -1,31 +1,3 @@
-
-var data = JSON.parse(data)
-console.log(data)
-
-
-function importIDB(dname, sname, arr) {
-  return new Promise(function(resolve) {
-    var r = window.indexedDB.open(dname)
-    r.onupgradeneeded = function() {
-      var idb = r.result
-      var store = idb.createObjectStore(sname, {keyPath: "name"})
-    }
-    r.onsuccess = function() {
-      var idb = r.result
-        let tactn = idb.transaction(sname, "readwrite")
-    	  var store = tactn.objectStore(sname)
-        for(var obj of arr) {
-          store.put(obj)
-        }
-        resolve(idb)
-    }
-    r.onerror = function (e) {
-     alert("Enable to access IndexedDB, " + e.target.errorCode)
-    }    
-  })
-}
-
-loadJSON("tables.json")
 let db;
 let dbReq = indexedDB.open('myDatabase', 1);
 
@@ -34,93 +6,76 @@ dbReq.onupgradeneeded = function(event) {
   var objectStore;
 
   //define initial table values
-  var cofdaily = {id: 1, name: 'cofdaily', fields: [{1: 'reg'}, {2: 'shipton'}], links: [{2:2}]};
-  var cmf = {id: 2, name: 'cmf', fields: [{2: 'cmfacctn'}, {3: 'mktgacctn'}], links: [{1:2}]};
-  var mktg = {id: 3, name: 'mktg', fields: [{3: 'mktgacctn'}, {4:'sourcedate'}], links: [{2:3}]};
-  //var tableObjects = [cofdaily, cmf, mktg];
-  
-  var cofJ = [
+  var cofdaily = [
     {
-      name: "cof",
+      tableName: "cofdaily",
       fieldNum: 1,
       fieldName: 'reg'
     },
     {
-      name: "cof",
+      tableName: "cofdaily",
       fieldNum: 2,
       fieldName: 'shipton'
     }
   ];
-  var cmfJ = [
-    {
-      name: "cmf",
-      fieldNum: 2,
-      fieldName: 'cmfacctn'
-    },
-    {
-      name: "cmf",
-      fieldNum: 3,
-      fieldName: 'mktgacctn'
-    }
-  ]
 
-  var tableObjects = [cofJ, cmfJ];
-  if (!db.objectStoreNames.contains('tables')) {
-  objectStore = db.createObjectStore('tables',  {keyPath: 'id'});
+  var cmf = [{tableName: "cmf",fieldNum: 2,fieldName: 'cmfacctn'},{tableName: "cmf",fieldNum: 3,fieldName: 'mktgacct'}];
+  var mktg = [{tableName: "mktg",fieldNum: 3,fieldName: 'mktgacct'},{tableName: "mktg",fieldNum: 4,fieldName: 'sourcedate'}];
+
+  var tableObjects = [cofdaily, cmf, mktg];
+
+  if (!db.objectStoreNames.contains('fields')) {
+  objectStore = db.createObjectStore('fields',  {keyPath: "id", autoIncrement:true });
   
   //define index
-  objectStore.createIndex('name', 'name', { unique: true});//, multiEntry: true 
+  objectStore.createIndex('tableName_idx', 'tableName');
+  objectStore.createIndex('fieldNum_idx', 'fieldNum');
+  objectStore.createIndex('fieldName_idx', 'fieldName');
 
   //doesn't look like indexing array is possible right now in indexeddb
   //objectStore.createIndex('fields', 'fields', { unique: false});
 
   for(var key in tableObjects) {
-    //objectStore.put(tableObjects[key]);
+    for (let i = 0; i < tableObjects[key].length; i++) {
+      objectStore.put(tableObjects[key][i]);
+      }
     }
-  //adds initial data (only once)
-  //objectStore.put(cofJ)
-  //objectStore.put(cmf)
   }
 }
 
 dbReq.onsuccess = function(event) {
-  db = event.target.result;
-  //for loop to loop through array of objects. object.field passed into addtable?
-  
-  //id - keyPath
-  //nm - tableName
-  //flds - fieldID, fieldName (local name of field)
-  //lnks - tableID, fieldID (of linkage)
-
-  //testing put
-  /*
-  var cofdaily = {id: 1, nm: 'cofdaily', flds: [{1: 'reg'}, {2: 'shipton'}], lnks: [{2:2}]};
-  var cmf = {id: 2, nm: 'cmf', flds: [{2: 'cmfacctn'}, {3: 'mktgacctn'}], lnks: [{1:2}]};
-  
-  //store tables as array of objects
-  var tableObjects = [cofdaily, cmf];
-  //loop through each table object to add to database
-  for(var key in tableObjects) {
-  addTable(db, tableObjects[key]);
-  }
-  */
-  //readTable(db);
-  //readAll(db);
-  //readFields();
-  //readAllFields(db);
+  getField();
 }
 
 dbReq.onerror = function(event) {
   alert('error opening database ' + event.target.errorCode);
 }
+function getField () {
+  db = event.target.result;
+  let transaction = db.transaction("fields");
+  let fields = transaction.objectStore("fields");
+  let fieldIndex = fields.index("fieldName_idx") ;
 
+  let request = fieldIndex.getAll('mktgacct');
+
+  request.onsuccess = function() {
+    if (request.result !== undefined) {
+      console.log("fields", request.result); // array of books with price=10
+    } else {
+      console.log("No such fields");
+    }
+  }
+}
 //to add transactions to tables objectStore
-function addTable (db, object) {
-  let tx = db.transaction(['tables'], 'readwrite');
-  let tableName = tx.objectStore('tables');
+function addTable () {
+  
+  let tx = db.transaction(['fields'], 'readwrite')
+  .objectStore('fields')
+  .add({fieldNum: 1, fieldName: 'reg', tableName: 'cofdaily'})
+  
 
-  let table = {id: object.id, name: object.nm, fields: object.flds, links: object.lnks};
-  tableName.add(table);
+  //let table = {id: object.id, name: object.nm, fields: object.flds, links: object.lnks};
+  //tableName.add(table);
 
   tx.oncomplete = function() { console.log('stored table!') }
   tx.onerror = function(event) {
@@ -147,19 +102,19 @@ function readTable(db) {
  };
 }
 
-function readAll(db) {
-  var objectStore = db.transaction('tables').objectStore('tables');
+function readAll() {
+  var objectStore = db.transaction('fields').objectStore('fields');
 
    objectStore.openCursor().onsuccess = function (event) {
      var cursor = event.target.result;
 
      if (cursor) {
        console.log('Id: ' + cursor.key);
-       console.log('Name: ' + cursor.value.name);
-       console.log('fields: ' + cursor.value.fields);
-       for(var key in cursor.value.fields) {
-       console.log('Links: ' + JSON.stringify(cursor.value.fields[key]));
-       }
+       console.log('Name: ' + cursor.value.tableName);
+       console.log('fields: ' + cursor.value.fieldName);
+       //for(var key in cursor.value.fields) {
+       //console.log('Links: ' + JSON.stringify(cursor.value.fields[key]));
+       //}
        cursor.continue();
     } else {
       console.log('No more data');
@@ -210,16 +165,5 @@ function readFields () {
     }
     htmlDiv.innerHTML = htmlOut;
   }
-
-  /*
-  request.onsuccess = function (e) {
-    var result = e.target.result;
-    if (result) {
-      console.log(result)
-    } else {
-      console.log("no fields")
-    }
-  }
-  */
 }
 
